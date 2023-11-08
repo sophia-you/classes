04c1d2ce214353de64507d90da17d7177250ae34
U-BSD\418807 <418807@SHS-BYBF6X3.beaverton.k12.or.us>
2023-11-08 09:16:12 -0800

    second commit

--- classesMain.cpp
+++ classesMain.cpp
@@ -44,6 +44,15 @@ int main()
       cout << "music" << endl;
       cout << "movies" << endl;
       cin.getline(input, max);
+      if (strcmp(input, "video games") == 0)
+	{
+	}
+      else if (strcmp(input, "music") == 0)
+        {
+        }
+      else if (strcmp(input, "movies") == 0)
+	{
+	}
       
     }
   else if (strcmp(input, "search") == 0)
@@ -60,4 +69,31 @@ int main()
 // this function takes in user input and puts the media into a vector
 void addMedia(vector<Parent*> &mediaList)
 {
+  int max = 80;
+  char input[max];
+
+  // specify the media type
+   cout << "Enter one of the following media types. Only use lower case lett\
+ers." << endl;
+      cout << "video games" << endl;
+      cout << "music" << endl;
+      cout << "movies" << endl;
+      cin.getline(input, max);
+      if (strcmp(input, "video games") == 0)
+        { // START HERE
+	  cout << "Title: " << endl;
+	  char* gameTitle = new char[100];
+	  strcpy(gameTitle, "hello");
+	  cout << "Year Released: " << endl;
+	  cout << "Publisher: " << endl;
+	  cout << "Rating: " << endl;
+	  VideoGames* videoGame = new VideoGames(); // this goes in the vector
+	  
+        }
+      else if (strcmp(input, "music") == 0)
+        {
+        }
+      else if (strcmp(input, "movies") == 0)
+        {
+        }
 }
